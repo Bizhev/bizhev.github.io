8daada9c383964f1f2b29bc40ef0f797756196a3
Dolet Bizhev <doletbizhev@gmail.com>
2018-04-30 11:10:24 +0300

    del gsap

--- src/js/common.js
+++ src/js/common.js
@@ -1,19 +1,15 @@
-import './app'
 import '../sass/main.sass'
 // import '../sass/media.css'
-import "gsap";
+// import "gsap";
 
 
-let t1 = new TimelineMax();
-t1
-  // .from(".DHeader", 2, {y: -100})
-  // .from(".text-content",2,{y: -300},"-=2")
-  .from(".ava-div",4,{y: -50},"-=1")
-  .from(".Idname",4,{y: -50},"-=1")
-  .from(".contact-list",2,{y: 20},"-=1")
-  .from(".social-list",2,{y: 20},"-=1")
+// let t1 = new TimelineMax();
+// t1
 
-  // .from(".div-txt",2,{y: 1000},"-=2")
-  // .to(bg2.scale, 1.5, { x: "+=0.018", y: "+=0.018" }, 1);
+//   .from(".ava-div",4,{y: -50},"-=1")
+//   .from(".Idname",4,{y: -50},"-=1")
+//   .from(".contact-list",2,{y: 20},"-=1")
+//   .from(".social-list",2,{y: 20},"-=1")
+
   console.log('window',window);
   console.log("location", window.location);
