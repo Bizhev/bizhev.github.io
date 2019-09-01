50879451a1beed54f421d7ceb3c559d7f0c684bf
Dolet B <doletbizhev@gmail.com>
2019-09-01 22:45:39 +0300

    remark

--- push.sh
+++ push.sh
@@ -10,5 +10,5 @@ git init
 git add -A
 git commit -m "$*"
 
-git push -f git@github.com:$autor/$autor.github.io.git master:vue
+git push -f git@github.com:$autor/$autor.github.io.git vue:vue
 # git push -f git@github.com:$autor/$repo.git master:master
