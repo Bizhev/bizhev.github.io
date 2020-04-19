be0297199069414b99e26ea47e32d29d6becebb4
Dolet B <doletbizhev@gmail.com>
2020-04-19 23:09:50 +0300

    deploy

---
+++ precache-manifest.8e260eaba73fec9d3a79774af6e64679.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144e7c72d868d995c462",
+    "url": "/css/app.24f55251.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "459ecbabc39f10520d55ac1a72e10709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "144e7c72d868d995c462",
+    "url": "/js/app.f01efd6e.js"
+  },
+  {
+    "revision": "524e7b2a63abdd09d9da",
+    "url": "/js/chunk-2d22d746.e7255521.js"
+  },
+  {
+    "revision": "da12907d8b4399a82fbb",
+    "url": "/js/chunk-vendors.5552428f.js"
+  },
+  {
+    "revision": "e5481b63f4cb0c60d4910dee8526d828",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
